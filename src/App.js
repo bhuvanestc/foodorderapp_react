1fbe47590c41154e47f275ed6cd044d0db1574ab
bhuvan <bhuvanapraap@gmail.com>
2024-01-03 18:38:07 +0100

    added dark mode and light mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
 import Cart from './components/Cart';
 import { CartProvider } from 'react-use-cart';
+
 const App = () => {
+    const [theme, setTheme] = useState('light'); 
+    const toggleTheme = () => {
+        setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+    const appStyle = {
+        backgroundColor: theme === 'dark' ? '#333' : '#fff', 
+        color: theme === 'dark' ? '#fff' : '#333', 
+        transition: 'background-color 0.3s ease-in-out, color 0.3s ease-in-out'
+    };
+
+    
     return (
-        <>
+        
+    <div style={appStyle}>
+    <button onClick={toggleTheme}>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+    </button>
             <CartProvider>
             <Home/>
             <Cart/>
             </CartProvider>
-        </>
+        </div>
+        
     );
 };
 
